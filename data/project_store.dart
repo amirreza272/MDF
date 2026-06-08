0c3ee08e0f959f216e4c91e6ec41dd5959eae564
amirreza272 <amirrezapiri99@gmail.com>
2026-06-08 16:11:01 +0330

    Add files via upload

---
+++ data/project_store.dart
@@ -0,0 +1,105 @@
+import 'fake_data.dart';
+import 'models/group_model.dart';
+import 'models/piece_model.dart';
+import 'models/project_model.dart';
+import 'models/sub_group_model.dart';
+
+class ProjectStore {
+  static final List<ProjectModel> _projects = [
+    ...fakeProjects,
+  ];
+
+  static List<ProjectModel> getProjects() {
+    return _projects;
+  }
+
+  static void addProject(ProjectModel project) {
+    _projects.add(project);
+  }
+
+  static void addGroup({
+    required String projectId,
+    required GroupModel group,
+  }) {
+    final project = _projects.firstWhere(
+      (p) => p.id == projectId,
+    );
+
+    project.groups.add(group);
+  }
+
+  static void addSubGroup({
+    required String projectId,
+    required String groupId,
+    required SubGroupModel subGroup,
+  }) {
+    final group = _findGroup(
+      projectId,
+      groupId,
+    );
+
+    group.subGroups.add(subGroup);
+  }
+
+  static void addPieceToGroup({
+    required String projectId,
+    required String groupId,
+    required PieceModel piece,
+  }) {
+    final group = _findGroup(
+      projectId,
+      groupId,
+    );
+
+    group.pieces.add(piece);
+  }
+
+  static void addPieceToSubGroup({
+    required String projectId,
+    required String groupId,
+    required String subGroupId,
+    required PieceModel piece,
+  }) {
+    final subGroup = _findSubGroup(
+      projectId,
+      groupId,
+      subGroupId,
+    );
+
+    subGroup.pieces.add(piece);
+  }
+
+  static void togglePiece(
+    PieceModel piece,
+  ) {
+    piece.completed = !piece.completed;
+  }
+
+  static GroupModel _findGroup(
+    String projectId,
+    String groupId,
+  ) {
+    final project = _projects.firstWhere(
+      (p) => p.id == projectId,
+    );
+
+    return project.groups.firstWhere(
+      (g) => g.id == groupId,
+    );
+  }
+
+  static SubGroupModel _findSubGroup(
+    String projectId,
+    String groupId,
+    String subGroupId,
+  ) {
+    final group = _findGroup(
+      projectId,
+      groupId,
+    );
+
+    return group.subGroups.firstWhere(
+      (s) => s.id == subGroupId,
+    );
+  }
+}

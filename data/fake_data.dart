08f2f96c33a27998b4c80206a8743902bb32ac00
amirreza272 <amirrezapiri99@gmail.com>
2026-06-11 17:06:46 +0330

    Update fake_data.dart

--- data/fake_data.dart
+++ data/fake_data.dart
@@ -12,7 +12,6 @@ final List<ProjectModel> fakeProjects = [
       GroupModel(
         id: '1',
         name: 'آشپزخانه',
-
         pieces: [
           PieceModel(
             id: '1',
@@ -23,16 +22,23 @@ final List<ProjectModel> fakeProjects = [
             completed: false,
             note: '',
           ),
+          PieceModel(
+            id: '2',
+            materialType: 'Fibr',
+            length: 68,
+            width: 38,
+            quantity: 2,
+            completed: false,
+            note: '',
+          ),
         ],
-
         subGroups: [
           SubGroupModel(
-            id: '1',
+            id: 'sg1',
             name: 'کشو 1',
-
             pieces: [
               PieceModel(
-                id: '2',
+                id: '3',
                 materialType: 'MDF',
                 length: 90,
                 width: 50,
@@ -40,10 +46,121 @@ final List<ProjectModel> fakeProjects = [
                 completed: true,
                 note: '',
               ),
+              PieceModel(
+                id: '4',
+                materialType: 'Fibr',
+                length: 65,
+                width: 53,
+                quantity: 2,
+                completed: false,
+                note: '',
+              ),
+            ],
+          ),
+          SubGroupModel(
+            id: 'sg2',
+            name: 'کشو 2',
+            pieces: [
+              PieceModel(
+                id: '5',
+                materialType: 'MDF',
+                length: 56,
+                width: 56,
+                quantity: 2,
+                completed: false,
+                note: '',
+              ),
+              PieceModel(
+                id: '6',
+                materialType: 'Fibr',
+                length: 68,
+                width: 68,
+                quantity: 1,
+                completed: false,
+                note: '',
+              ),
+            ],
+          ),
+          SubGroupModel(
+            id: 'sg3',
+            name: 'درب ها',
+            pieces: [
+              PieceModel(
+                id: '7',
+                materialType: 'MDF',
+                length: 68,
+                width: 68,
+                quantity: 2,
+                completed: false,
+                note: '',
+              ),
+              PieceModel(
+                id: '8',
+                materialType: 'MDF',
+                length: 68,
+                width: 68,
+                quantity: 2,
+                completed: false,
+                note: '',
+              ),
+              PieceModel(
+                id: '9',
+                materialType: 'Fibr',
+                length: 68,
+                width: 35,
+                quantity: 38,
+                completed: false,
+                note: '',
+              ),
             ],
           ),
         ],
-      )
+      ),
+      GroupModel(
+        id: '2',
+        name: 'اتاق خواب',
+        pieces: [],
+        subGroups: [
+          SubGroupModel(
+            id: 'sg4',
+            name: 'کمد',
+            pieces: [
+              PieceModel(
+                id: '10',
+                materialType: 'MDF',
+                length: 200,
+                width: 60,
+                quantity: 2,
+                completed: false,
+                note: '',
+              ),
+            ],
+          ),
+        ],
+      ),
+    ],
+  ),
+  ProjectModel(
+    id: '2',
+    name: 'پروژه رضایی',
+    createdAt: DateTime.now(),
+    groups: [
+      GroupModel(
+        id: '3',
+        name: 'پذیرایی',
+        pieces: [
+          PieceModel(
+            id: '11',
+            materialType: 'MDF',
+            length: 120,
+            width: 40,
+            quantity: 4,
+            completed: true,
+            note: '',
+          ),
+        ],
+        subGroups: [],
+      ),
     ],
   ),
 ];

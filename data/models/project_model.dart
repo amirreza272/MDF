633bcd120d7d460a77a3ce3802521d414114b566
amirreza272 <amirrezapiri99@gmail.com>
2026-06-11 17:07:53 +0330

    Update project_model.dart

--- data/models/project_model.dart
+++ data/models/project_model.dart
@@ -2,17 +2,14 @@ import 'group_model.dart';
 
 class ProjectModel {
   final String id;
-
-  final String name;
-
+  String name;
   final DateTime createdAt;
-
-  final List<GroupModel> groups;
+  List<GroupModel> groups;
 
   ProjectModel({
     required this.id,
     required this.name,
     required this.createdAt,
-    required this.groups,
-  });
+    List<GroupModel>? groups,
+  }) : groups = groups ?? [];
 }
